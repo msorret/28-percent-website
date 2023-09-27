6b1e4f66ac6f179af4947d2ac3faa43026cbc6fa
Ms. Orret <71471052+msorret@users.noreply.github.com>
2023-09-26 17:21:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var form = document.getElementById("my-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("my-form"))
+  })
+    .then((response) => response.json())
+    .then((html) => {
+      // you can put any JS code here
+
+      document.getElementById("my-form").reset();
+      
+    });
+});
 
